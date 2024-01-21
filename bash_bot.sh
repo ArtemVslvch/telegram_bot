747271d60798a5c99f4ba490166ef8617b9fb1ae
ArtemVslvch <119794207+ArtemVslvch@users.noreply.github.com>
2024-01-21 18:58:29 +1000

    version: 2

--- bash_bot.sh
+++ bash_bot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Поправь логирование
+#Переработай сбор метрик
+#Добавь service systemd
+#добавь метод остановки
+
 #vars
 TOKEN="6856700358:AAECiPV0PQmmDxmUSEBAQVsWt3Dxjj4yoms"
 CHAT_ID="566855756"
@@ -12,36 +17,79 @@ log() {
 }
 
 
-#SEND message
-##logs
-log "$message"
-##send
-curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$message" >> curl.log  2>&1
-
-
-#GET message
-##get offset old message
-offset=$(cat "$offset_file")
-
-##get message
-updates=$(curl -s "https://api.telegram.org/bot$TOKEN/getUpdates?offset=$offset")
-
-##save respone
-echo "$updates" > response.log
-
-##count message in updates
-message_count=$( echo "$updates" | jq '.result | length' )
-
-##parsing message in cicle
-if [ $message_count -gt 0 ]
-then
-for (( i=1; i < message_count; i++ )){
-  text=$(echo "$updates" | jq ".result[$i].message.text" | tr -d \")
-  echo "$text"
+send_message() {
+  curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$1" >> curl.log  2>&1
 }
-##save offset
-echo "$updates" | jq ".result[-1].update_id"  > $offset_file
-fi
 
+get_message() {
+  #GET message
+  ##get offset old message
+  offset=$(cat "$offset_file")
+
+  ##get message
+  updates=$(curl -s "https://api.telegram.org/bot$TOKEN/getUpdates?offset=$offset")
+
+  ##save respone
+  echo "$updates" > response.log
+
+  ##count message in updates
+  message_count=$( echo "$updates" | jq '.result | length' )
+
+  ##parsing message in cicle
+  if [ $message_count -gt 0 ]
+  then
+  for (( i=1; i < message_count; i++ )){
+    text=$(echo "$updates" | jq ".result[$i].message.text" | tr -d \")
+    commands "$text"
+    echo "$text"
+  }
+  ##save offset
+  echo "$updates" | jq ".result[-1].update_id"  > $offset_file
+  fi
+}
+
+send_command_output() {
+  local command_output="$($1)"
+  send_message "$command_output"
+  log "$command_output"
+}
+
+commands() {
+  local command="$1"
+  case "$command" in
+    "/start")
+      send_message "Available commands:
+1. /start
+2. /memory
+3. /df
+4. /cpu"
+      ;;
+
+    "/memory")
+      send_command_output "free -m"
+      ;;
+
+    "/df")
+      send_command_output "df -h"
+      ;;
+
+    "/cpu")
+      #send_command_output "top -bn1 | awk '/Cpu/ { print $2}'"
+      ;;
+
+    *)
+      send_message "No matching option found for: $command"
+      log "No matching option found for: $command"
+      ;;
+  esac
+}
+
+
+#######LOOP#############
+while true; do
+  get_message
+  sleep 5
+
+done
 
 
